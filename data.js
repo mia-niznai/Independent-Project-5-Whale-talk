97f8270ab0d7beab510f23ec26c47dfe1e99eae8
mia-niznai <maria.popa.8735@gmail.com>
2022-12-23 09:11:28 +0200

    whaleIsh language

--- data.js
+++ data.js
@@ -6,3 +6,22 @@ let vowels = ["a", "e", "i", "o", "u"];
 
 //Result array
 let resultArray = [];
+let whaleIsh = [];
+
+//Double the vowels
+for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+        if (input[i] === vowels[j]) {
+            resultArray.push(input[i]);
+            //console.log(input[i])
+        }
+    }
+    if (input[i]=== "e"){
+        resultArray.push("ee");
+    } if (input[i] === "u"){
+        resultArray.push("uu");
+        console.log(resultArray)
+    }
+}
+
+console.log(resultArray)
